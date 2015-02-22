754c8dbbbb62baac98f686f3806e0415f9cd9af5
Arjun Venkataswamy <arjun@example.com>
2015-02-21 18:18:08 -0600

    creates janky rating solution

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review < ActiveRecord::Base
   belongs_to :episode
   belongs_to :user
+  after_save :calcuate_average_rating
+
+  private
+    def calcuate_average_rating
+      episode = self.episode
+      ep_ratings = episode.reviews.map do |review|
+        review.rating
+      end
+
+      episode.rating = ep_ratings.sum / ep_ratings.size.to_f
+      episode.save
+    end
 end
+
+
