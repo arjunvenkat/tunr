16fcc3b486a62805b37d1753b574a45cfeff8519
student <student@tsl-mbp-01.attlocal.net>
2015-02-27 09:55:49 -0600

    adds activity to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def home
+    @recent_reviews = current_user.follows.map do |user|
+      user.reviews.order('created_at DESC').limit(3)
+    end.flatten.sort_by{ |review| review.created_at }.take(24)
+
     @episodes = Episode
                   .where.not(rating: nil)
                   .order('rating DESC')
