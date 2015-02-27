60df788bf03b637528cd3900462daecd3dc7830a
student <student@tsl-mbp-01.attlocal.net>
2015-02-27 12:51:18 -0600

    temp fix for unsigned in users on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,11 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def home
-    @recent_reviews = current_user.follows.map do |user|
-      user.reviews.order('created_at DESC').limit(3)
-    end.flatten.sort_by{ |review| review.created_at }.take(24)
+    if user_signed_in?
+      @recent_reviews = current_user.follows.map do |user|
+        user.reviews.order('created_at DESC').limit(3)
+      end.flatten.sort_by{ |review| review.created_at }.take(24)
+    end
 
     @episodes = Episode
                   .where.not(rating: nil)
