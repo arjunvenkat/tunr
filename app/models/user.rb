757ab8a3560e89ad322cacea757f6136fa9e914a
student <student@TSL-MBP-01.local>
2015-02-28 17:10:27 -0600

    adds follow suggestions on home page for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :upvotes, dependent: :destroy
 
+  before_create :add_profile_pic
+
   def followers
     Following.where(followed_id: self.id).map { |following| following.follower }
   end
@@ -19,4 +21,10 @@ class User < ActiveRecord::Base
 
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
+
+  def add_profile_pic
+    if self.image_url.blank?
+      self.image_url = Faker::Avatar.image(self.username)
+    end
+  end
 end
