19f7c86ae3673f8d0369004f8fa94a096f69b5b7
student <student@tsl-mbp-01.attlocal.net>
2015-03-03 12:18:05 -0600

    adds avg rating to podcast model

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,4 +1,12 @@
 class Podcast < ActiveRecord::Base
   has_many :episodes, dependent: :destroy
   has_many :reviews, through: :episodes
+
+  def avg_rating
+    total_rating = self.episodes.inject(0) do |sum, ep|
+      sum + ep.rating
+    end
+
+    return total_rating/(self.episodes.count)
+  end
 end
